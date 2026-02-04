e35e95a627c9281855d1f57006f31093206790ee
Senuda Damnidu Karunarathne Adihetty <senuadihetty429@gmail.com>
2026-02-04 23:30:07 +0530

    feat: add scroll-to-top and scroll-to-bottom navigation buttons

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import WhatsAppButton from "../WhatsAppButton";
+import ScrollNavigation from "../ScrollNavigation";
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,6 +16,7 @@ const Layout = ({ children }: LayoutProps) => {
         {children}
       </main>
       <Footer />
+      <ScrollNavigation />
       <WhatsAppButton />
     </div>
   );
